8206c83b3847a8bc461f86e547306be2145cf762
ty-mike18 <yoshizaki@toyo.jp>
2026-06-15 18:11:39 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME = 'tonari-v20260615-final-v3-22-flat';
+const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png', './sakana.png', './abura.png', './niku.png', './gyunyu.png', './yasai.png', './kaiso.png', './imo.png', './tamago.png', './daizu.png', './kudamono.png', './confection.png', './sweet_drink.png', './weight_scale.png', './bedtime.png', './wake_time.png', './weight_paw.png', './wata-body.png', './wata-tail.png'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', event => { if(event.request.method !== 'GET') return; event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))); });
